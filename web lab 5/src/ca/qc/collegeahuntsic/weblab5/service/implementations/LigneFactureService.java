232967086fe3992d70a08aecd7c7d1e989cdb5b3
Félix Brunet <brunetfelix@outlook.com>
2014-11-09 14:03:32 -0500

    Qulify avec this

--- web lab 5/src/ca/qc/collegeahuntsic/weblab5/service/implementations/LigneFactureService.java
+++ web lab 5/src/ca/qc/collegeahuntsic/weblab5/service/implementations/LigneFactureService.java
@@ -1,7 +1,7 @@
+
 package ca.qc.collegeahuntsic.weblab5.service.implementations;
 
 import java.util.List;
-
 import ca.qc.collegeahuntsic.weblab5.bean.AchatBean;
 import ca.qc.collegeahuntsic.weblab5.bean.LigneFactureBean;
 import ca.qc.collegeahuntsic.weblab5.dao.interfaces.ILigneFactureDAO;
@@ -11,69 +11,75 @@ import ca.qc.collegeahuntsic.weblab5.exception.service.ServiceException;
 import ca.qc.collegeahuntsic.weblab5.service.interfaces.ILigneFactureService;
 
 public class LigneFactureService extends Service implements ILigneFactureService {
-	
-	private ILigneFactureDAO ligneFactureDAO;
-	
-	public LigneFactureService(ILigneFactureDAO ligneFactureDAO){
-		super();
-		setLigneFactureDAO(ligneFactureDAO);
-	}
 
-	public ILigneFactureDAO getLigneFactureDAO() {
-		return ligneFactureDAO;
-	}
+    private ILigneFactureDAO ligneFactureDAO;
 
-	public void setLigneFactureDAO(ILigneFactureDAO ligneFactureDAO) {
-		this.ligneFactureDAO = ligneFactureDAO;
-	}
+    public LigneFactureService(ILigneFactureDAO ligneFactureDAO) {
+        super();
+        setLigneFactureDAO(ligneFactureDAO);
+    }
 
-	@Override
-	public LigneFactureBean add(Connexion connexion,
-			LigneFactureBean ligneFactureBean) throws ServiceException {
-		try {
-			return (LigneFactureBean) getLigneFactureDAO().add(connexion, ligneFactureBean);
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
-	}
+    public ILigneFactureDAO getLigneFactureDAO() {
+        return this.ligneFactureDAO;
+    }
 
-	@Override
-	public LigneFactureBean get(Connexion connexion,
-			LigneFactureBean ligneFactureBean) throws ServiceException {
-		try {
-			return (LigneFactureBean) getLigneFactureDAO().get(connexion, ligneFactureBean.getIdLigneFacture());
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
-	}
+    public void setLigneFactureDAO(ILigneFactureDAO ligneFactureDAO) {
+        this.ligneFactureDAO = ligneFactureDAO;
+    }
 
-	@Override
-	public void update(Connexion connexion, LigneFactureBean ligneFactureBean) throws ServiceException {
-		try {
-			getLigneFactureDAO().update(connexion, ligneFactureBean);
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
-	}
+    @Override
+    public LigneFactureBean add(Connexion connexion,
+        LigneFactureBean ligneFactureBean) throws ServiceException {
+        try {
+            return (LigneFactureBean) getLigneFactureDAO().add(connexion,
+                ligneFactureBean);
+        } catch(DAOException e) {
+            throw new ServiceException(e);
+        }
+    }
 
-	@Override
-	public void delete(Connexion connexion, LigneFactureBean ligneFactureBean) throws ServiceException {
-		try {
-			getLigneFactureDAO().delete(connexion, ligneFactureBean);
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
-	}
+    @Override
+    public LigneFactureBean get(Connexion connexion,
+        LigneFactureBean ligneFactureBean) throws ServiceException {
+        try {
+            return (LigneFactureBean) getLigneFactureDAO().get(connexion,
+                ligneFactureBean.getIdLigneFacture());
+        } catch(DAOException e) {
+            throw new ServiceException(e);
+        }
+    }
 
-	@Override
-	public List<LigneFactureBean> findByAchat(Connexion connexion,
-			AchatBean achatBean) throws ServiceException {
-		try {
-			return getLigneFactureDAO().findByAchat(connexion, achatBean);
-		} catch (DAOException e) {
-			throw new ServiceException(e);
-		}
-	}
+    @Override
+    public void update(Connexion connexion,
+        LigneFactureBean ligneFactureBean) throws ServiceException {
+        try {
+            getLigneFactureDAO().update(connexion,
+                ligneFactureBean);
+        } catch(DAOException e) {
+            throw new ServiceException(e);
+        }
+    }
 
+    @Override
+    public void delete(Connexion connexion,
+        LigneFactureBean ligneFactureBean) throws ServiceException {
+        try {
+            getLigneFactureDAO().delete(connexion,
+                ligneFactureBean);
+        } catch(DAOException e) {
+            throw new ServiceException(e);
+        }
+    }
+
+    @Override
+    public List<LigneFactureBean> findByAchat(Connexion connexion,
+        AchatBean achatBean) throws ServiceException {
+        try {
+            return getLigneFactureDAO().findByAchat(connexion,
+                achatBean);
+        } catch(DAOException e) {
+            throw new ServiceException(e);
+        }
+    }
 
 }
