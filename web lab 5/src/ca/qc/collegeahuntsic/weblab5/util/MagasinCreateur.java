09bfab68bb6c2182bf87fa0e94ee7b20c648c1ee
jonathan aumont <jonathan.aumont.1@collegeahuntsic.qc.ca>
2014-11-08 21:38:57 -0500

    allo

--- web lab 5/src/ca/qc/collegeahuntsic/weblab5/util/MagasinCreateur.java
+++ web lab 5/src/ca/qc/collegeahuntsic/weblab5/util/MagasinCreateur.java
@@ -3,8 +3,20 @@ package ca.qc.collegeahuntsic.weblab5.util;
 import java.io.Serializable;
 import java.sql.SQLException;
 
-import ca.qc.collegeahuntsic.weblab5.bean.*;
-import ca.qc.collegeahuntsic.weblab5.dao.implementations.*;
+import ca.qc.collegeahuntsic.weblab5.bean.AchatBean;
+import ca.qc.collegeahuntsic.weblab5.bean.ClientBean;
+import ca.qc.collegeahuntsic.weblab5.bean.LigneFactureBean;
+import ca.qc.collegeahuntsic.weblab5.bean.LignePanierBean;
+import ca.qc.collegeahuntsic.weblab5.bean.ProduitBean;
+import ca.qc.collegeahuntsic.weblab5.bean.ProfilBean;
+import ca.qc.collegeahuntsic.weblab5.bean.StockBean;
+import ca.qc.collegeahuntsic.weblab5.dao.implementations.AchatDAO;
+import ca.qc.collegeahuntsic.weblab5.dao.implementations.ClientDAO;
+import ca.qc.collegeahuntsic.weblab5.dao.implementations.LigneFactureDAO;
+import ca.qc.collegeahuntsic.weblab5.dao.implementations.LignePanierDAO;
+import ca.qc.collegeahuntsic.weblab5.dao.implementations.ProduitDAO;
+import ca.qc.collegeahuntsic.weblab5.dao.implementations.ProfilDAO;
+import ca.qc.collegeahuntsic.weblab5.dao.implementations.StockDAO;
 import ca.qc.collegeahuntsic.weblab5.db.Connexion;
 import ca.qc.collegeahuntsic.weblab5.exception.MagasinException;
 import ca.qc.collegeahuntsic.weblab5.exception.db.ConnexionException;
@@ -27,70 +39,75 @@ import ca.qc.collegeahuntsic.weblab5.service.interfaces.IStockService;
 public class MagasinCreateur implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Connexion connexion;
-	
+
 	private IAchatService achatService;
-	
+
 	private IClientService clientService;
-	
-	private ILignePanierService	lignePanierService;
-	
+
+	private ILignePanierService lignePanierService;
+
 	private IProduitService produitService;
-	
+
 	private IProfilService profilService;
-	
+
 	private IStockService stockService;
-	
+
 	private ILigneFactureService ligneFactureService;
 
-	public MagasinCreateur (String serveur, String bd, String user, String password) throws MagasinException{
-		
+	public MagasinCreateur(String serveur, String bd, String user,
+			String password) throws MagasinException {
+
 		super();
-		
+
 		try {
-			
+
 			setConnexion(new Connexion(serveur, bd, user, password));
 			setAchatService(new AchatService(new AchatDAO(AchatBean.class)));
-			setClientService(new ClientService(new ClientDAO(ClientBean.class), new ProfilDAO(ProfilBean.class)));
-			setLignePanierService(new LignePanierService(new LignePanierDAO(LignePanierBean.class)));
-			setProduitService(new ProduitService(new ProduitDAO(ProduitBean.class)));
+			setClientService(new ClientService(new ClientDAO(ClientBean.class),
+					new ProfilDAO(ProfilBean.class)));
+			setLignePanierService(new LignePanierService(new LignePanierDAO(
+					LignePanierBean.class)));
+			setProduitService(new ProduitService(new ProduitDAO(
+					ProduitBean.class)));
 			setProfilService(new ProfilService(new ProfilDAO(ProfilBean.class)));
 			setStockService(new StockService(new StockDAO(StockBean.class)));
-			setLigneFactureService(new LigneFactureService(new LigneFactureDAO(LigneFactureBean.class)));
-		
+			setLigneFactureService(new LigneFactureService(new LigneFactureDAO(
+					LigneFactureBean.class)));
+
 		} catch (ConnexionException | InvalidDTOClassException e) {
 			throw new MagasinException(e);
 		}
-		
+
 	}
 
-	public void close() throws MagasinException{
+	public void close() throws MagasinException {
 		try {
 			getConnexion().close();
 		} catch (SQLException e) {
 			throw new MagasinException(e);
 		}
 	}
-	
-	public void commit() throws MagasinException{
+
+	public void commit() throws MagasinException {
 		try {
 			getConnexion().commit();
 		} catch (SQLException e) {
 			throw new MagasinException(e);
 		}
 	}
-	
-	public void rollback() throws MagasinException{
+
+	public void rollback() throws MagasinException {
 		try {
 			getConnexion().rollback();
 		} catch (SQLException e) {
 			throw new MagasinException(e);
 		}
 	}
-	
+
 	public Connexion getConnexion() {
-		return connexion;
+		return this.connexion;
 	}
 
 	public void setConnexion(Connexion connexion) {
@@ -98,7 +115,7 @@ public class MagasinCreateur implements Serializable {
 	}
 
 	public IAchatService getAchatService() {
-		return achatService;
+		return this.achatService;
 	}
 
 	public void setAchatService(IAchatService achatService) {
@@ -106,7 +123,7 @@ public class MagasinCreateur implements Serializable {
 	}
 
 	public IClientService getClientService() {
-		return clientService;
+		return this.clientService;
 	}
 
 	public void setClientService(IClientService clientService) {
@@ -114,7 +131,7 @@ public class MagasinCreateur implements Serializable {
 	}
 
 	public ILignePanierService getLignePanierService() {
-		return lignePanierService;
+		return this.lignePanierService;
 	}
 
 	public void setLignePanierService(ILignePanierService lignePanierService) {
@@ -122,7 +139,7 @@ public class MagasinCreateur implements Serializable {
 	}
 
 	public IProduitService getProduitService() {
-		return produitService;
+		return this.produitService;
 	}
 
 	public void setProduitService(IProduitService produitService) {
@@ -130,7 +147,7 @@ public class MagasinCreateur implements Serializable {
 	}
 
 	public IProfilService getProfilService() {
-		return profilService;
+		return this.profilService;
 	}
 
 	public void setProfilService(IProfilService profilService) {
@@ -138,7 +155,7 @@ public class MagasinCreateur implements Serializable {
 	}
 
 	public IStockService getStockService() {
-		return stockService;
+		return this.stockService;
 	}
 
 	public void setStockService(IStockService stockService) {
@@ -146,7 +163,7 @@ public class MagasinCreateur implements Serializable {
 	}
 
 	public ILigneFactureService getLigneFactureService() {
-		return ligneFactureService;
+		return this.ligneFactureService;
 	}
 
 	public void setLigneFactureService(ILigneFactureService ligneFactureService) {
