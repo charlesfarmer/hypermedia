8e821ae7b954d13098c2f13115a006fa072e391c
Félix <Flix@192.168.1.115>
2014-11-08 22:15:37 -0500

    Switch la méthode achat vers AchatFacade

--- web lab 5/src/ca/qc/collegeahuntsic/weblab5/facade/interfaces/IAchatFacade.java
+++ web lab 5/src/ca/qc/collegeahuntsic/weblab5/facade/interfaces/IAchatFacade.java
@@ -1,5 +1,9 @@
 package ca.qc.collegeahuntsic.weblab5.facade.interfaces;
 
-public interface IAchatFacade extends IFacade {
+import ca.qc.collegeahuntsic.weblab5.bean.ClientBean;
+import ca.qc.collegeahuntsic.weblab5.db.Connexion;
+import ca.qc.collegeahuntsic.weblab5.exception.facade.FacadeException;
 
+public interface IAchatFacade extends IFacade {
+	void acheter(Connexion connexion, ClientBean clientBean) throws FacadeException;
 }
--- web lab 5/src/ca/qc/collegeahuntsic/weblab5/facade/interfaces/ILigneFactureFacade.java
+++ web lab 5/src/ca/qc/collegeahuntsic/weblab5/facade/interfaces/ILigneFactureFacade.java
@@ -1,9 +1,6 @@
 package ca.qc.collegeahuntsic.weblab5.facade.interfaces;
 
-import ca.qc.collegeahuntsic.weblab5.bean.ClientBean;
-import ca.qc.collegeahuntsic.weblab5.db.Connexion;
-import ca.qc.collegeahuntsic.weblab5.exception.facade.FacadeException;
 
 public interface ILigneFactureFacade {
-	void acheter(Connexion connexion, ClientBean clientBean) throws FacadeException;
+	
 }
