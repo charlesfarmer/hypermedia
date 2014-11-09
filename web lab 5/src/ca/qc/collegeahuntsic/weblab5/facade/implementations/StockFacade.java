cb5674397d92334361d275d7c07d4a11856a7ffa
charlesfarmer <200675851@collegeahuntsic.qc.ca>
2014-11-08 21:18:12 -0500

    .gitignore ignore *.class

--- web lab 5/src/ca/qc/collegeahuntsic/weblab5/facade/implementations/StockFacade.java
+++ web lab 5/src/ca/qc/collegeahuntsic/weblab5/facade/implementations/StockFacade.java
@@ -4,13 +4,17 @@ import ca.qc.collegeahuntsic.weblab5.facade.interfaces.IStockFacade;
 import ca.qc.collegeahuntsic.weblab5.service.interfaces.IStockService;
 
 public class StockFacade extends Facade implements IStockFacade {
+
 	private IStockService service;
-	public StockFacade(IStockService service){
+
+	public StockFacade(IStockService service) {
 		setService(service);
 	}
+
 	private IStockService getService() {
 		return service;
 	}
+
 	private void setService(IStockService service) {
 		this.service = service;
 	}
