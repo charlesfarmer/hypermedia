e71fd771c23b37943483525f9efd7cbdc5e6b8b0
charles farmer <200675851@collegeahuntsic.qc.ca>
2014-11-10 22:37:24 -0500

    Update profil/profil.jsp

---
+++ web lab 5 - front-end/src/ca/qc/collegeahuntsic/weblab5/controller/ProfilServlet.java
@@ -0,0 +1,58 @@
+
+package ca.qc.collegeahuntsic.weblab5.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProfilServlet
+ */
+@WebServlet(
+    name = "profil",
+    urlPatterns = {"/profil"})
+public class ProfilServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProfilServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException,
+        IOException {
+        processRequest(request,
+            response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException,
+        IOException {
+        processRequest(request,
+            response);
+    }
+
+    @SuppressWarnings("static-method")
+    private void processRequest(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException,
+        IOException {
+        request.getRequestDispatcher("/WEB-INF/profil.jsp").forward(request,
+            response);
+    }
+
+}
