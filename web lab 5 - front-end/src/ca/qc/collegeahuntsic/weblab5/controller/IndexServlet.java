e78e6f6b68147d9ee847cf8418c862036387ad19
charles farmer <200675851@collegeahuntsic.qc.ca>
2014-11-10 20:14:31 -0500

    asdf

---
+++ web lab 5 - front-end/src/ca/qc/collegeahuntsic/weblab5/controller/IndexServlet.java
@@ -0,0 +1,66 @@
+
+package ca.qc.collegeahuntsic.weblab5.controller;
+
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet(
+    name = "index",
+    urlPatterns = {"/index"})
+public class IndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see Servlet#init(ServletConfig)
+     */
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException,
+        IOException {
+        processRequest(request,
+            response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException,
+        IOException {
+        processRequest(request,
+            response);
+    }
+
+    private void processRequest(HttpServletRequest request,
+        HttpServletResponse response) throws ServletException,
+        IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request,
+            response);
+    }
+}
