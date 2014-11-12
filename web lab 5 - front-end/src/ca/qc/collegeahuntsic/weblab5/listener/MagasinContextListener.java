0f38b1795c06516037a566801cf90de41faff292
Charles <Charles@PC_Charles>
2014-11-12 18:41:53 -0500

    retirer le 'ajouterClient' du ContextListener

--- web lab 5 - front-end/src/ca/qc/collegeahuntsic/weblab5/listener/MagasinContextListener.java
+++ web lab 5 - front-end/src/ca/qc/collegeahuntsic/weblab5/listener/MagasinContextListener.java
@@ -3,11 +3,7 @@ package ca.qc.collegeahuntsic.weblab5.listener;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import ca.qc.collegeahuntsic.weblab5.bean.ClientBean;
-import ca.qc.collegeahuntsic.weblab5.bean.ProfilBean;
 import ca.qc.collegeahuntsic.weblab5.exception.MagasinException;
-import ca.qc.collegeahuntsic.weblab5.exception.facade.FacadeException;
-import ca.qc.collegeahuntsic.weblab5.exception.service.EmailAlreadyUsedException;
 import ca.qc.collegeahuntsic.weblab5.util.MagasinCreateur;
 
 public class MagasinContextListener implements ServletContextListener {
@@ -15,7 +11,6 @@ public class MagasinContextListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
@@ -28,19 +23,7 @@ public class MagasinContextListener implements ServletContextListener {
                 "tiger");
             servletContextEvent.getServletContext().setAttribute("magasin",
                 magasin);
-            ProfilBean profilBean = new ProfilBean();
-            profilBean.setNom("Zoe");
-            profilBean.setPrenom("A");
-            ClientBean clientBean = new ClientBean();
-            clientBean.setEmail("a@a.a");
-            clientBean.setPassword("abc");
-            clientBean.setProfilBean(profilBean);
-            magasin.getClientFacade().ajouterClient(magasin.getConnexion(),
-                clientBean);
-        } catch(
-            MagasinException
-            | FacadeException
-            | EmailAlreadyUsedException e) {
+        } catch(MagasinException e) {
             e.printStackTrace();
         }
     }
