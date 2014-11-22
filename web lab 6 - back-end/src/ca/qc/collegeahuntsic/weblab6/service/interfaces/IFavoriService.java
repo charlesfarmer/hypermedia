f720b8c14902df0f80cd0acdf7e3ff32f48ed813
charlesfarmer <200675851@collegeahuntsic.qc.ca>
2014-11-22 08:54:47 -0500

    asdf

--- web lab 6 - back-end/src/ca/qc/collegeahuntsic/weblab6/service/interfaces/IFavoriService.java
+++ web lab 6 - back-end/src/ca/qc/collegeahuntsic/weblab6/service/interfaces/IFavoriService.java
@@ -1,39 +1,40 @@
-
 package ca.qc.collegeahuntsic.weblab6.service.interfaces;
 
 import java.util.List;
+
+import org.hibernate.Session;
+
 import ca.qc.collegeahuntsic.weblab6.dto.FavoriDTO;
+import ca.qc.collegeahuntsic.weblab6.exception.dao.FavoriAlreadyAddedException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidDTOException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidHibernateSessionException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidPrimaryKeyException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidSortByPropertyException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.ServiceException;
-import org.hibernate.Session;
 
 public interface IFavoriService extends IService {
 
-    List<FavoriDTO> getAllFavoris(Session session,
-        String sortByPropertyName) throws InvalidHibernateSessionException,
-        InvalidSortByPropertyException,
-        ServiceException;
+	List<FavoriDTO> getAllFavoris(Session session, String sortByPropertyName)
+			throws InvalidHibernateSessionException,
+			InvalidSortByPropertyException, ServiceException;
 
-    void deleteFavori(Session session,
-        FavoriDTO favoriDTO) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        ServiceException;
+	void deleteFavori(Session session, FavoriDTO favoriDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ServiceException;
 
-    void updateFavori(Session session,
-        FavoriDTO favoriDTO) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        ServiceException;
+	void updateFavori(Session session, FavoriDTO favoriDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ServiceException;
 
-    FavoriDTO getFavori(Session session,
-        String idFavori) throws InvalidHibernateSessionException,
-        InvalidPrimaryKeyException,
-        ServiceException;
+	FavoriDTO getFavori(Session session, String idFavori)
+			throws InvalidHibernateSessionException,
+			InvalidPrimaryKeyException, ServiceException;
 
-    void addFavori(Session session,
-        FavoriDTO favoriDTO) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        ServiceException;
+	void addFavori(Session session, FavoriDTO favoriDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ServiceException;
+
+	void ajouterFavori(Session session, FavoriDTO favoriDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			FavoriAlreadyAddedException, ServiceException;
 }
