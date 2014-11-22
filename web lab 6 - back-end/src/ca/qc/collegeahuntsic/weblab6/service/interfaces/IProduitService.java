7c94d3742e79b6485bfd70231bf3d9f67d4cc423
charlesfarmer <200675851@collegeahuntsic.qc.ca>
2014-11-22 09:08:43 -0500

    asdf

--- web lab 6 - back-end/src/ca/qc/collegeahuntsic/weblab6/service/interfaces/IProduitService.java
+++ web lab 6 - back-end/src/ca/qc/collegeahuntsic/weblab6/service/interfaces/IProduitService.java
@@ -1,39 +1,40 @@
-
 package ca.qc.collegeahuntsic.weblab6.service.interfaces;
 
 import java.util.List;
+
+import org.hibernate.Session;
+
 import ca.qc.collegeahuntsic.weblab6.dto.ProduitDTO;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidDTOException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidHibernateSessionException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidPrimaryKeyException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidSortByPropertyException;
+import ca.qc.collegeahuntsic.weblab6.exception.dao.ProduitAlreadyAddedException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.ServiceException;
-import org.hibernate.Session;
 
 public interface IProduitService extends IService {
 
-    List<ProduitDTO> getAllProduits(Session session,
-        String sortByPropertyName) throws InvalidHibernateSessionException,
-        InvalidSortByPropertyException,
-        ServiceException;
+	List<ProduitDTO> getAllProduits(Session session, String sortByPropertyName)
+			throws InvalidHibernateSessionException,
+			InvalidSortByPropertyException, ServiceException;
 
-    void deleteProduit(Session session,
-        ProduitDTO produitDTO) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        ServiceException;
+	void deleteProduit(Session session, ProduitDTO produitDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ServiceException;
 
-    void updateProduit(Session session,
-        ProduitDTO produitDTO) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        ServiceException;
+	void updateProduit(Session session, ProduitDTO produitDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ServiceException;
 
-    ProduitDTO getProduit(Session session,
-        String idProduit) throws InvalidHibernateSessionException,
-        InvalidPrimaryKeyException,
-        ServiceException;
+	ProduitDTO getProduit(Session session, String idProduit)
+			throws InvalidHibernateSessionException,
+			InvalidPrimaryKeyException, ServiceException;
 
-    void addProduit(Session session,
-        ProduitDTO produitDTO) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        ServiceException;
+	void addProduit(Session session, ProduitDTO produitDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ServiceException;
+
+	void ajouterProduit(Session session, ProduitDTO produitDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ProduitAlreadyAddedException, ServiceException;
 }
--- web lab 6 - back-end/src/ca/qc/collegeahuntsic/weblab6/service/interfaces/IVitrineService.java
+++ web lab 6 - back-end/src/ca/qc/collegeahuntsic/weblab6/service/interfaces/IVitrineService.java
@@ -1,40 +1,41 @@
-
 package ca.qc.collegeahuntsic.weblab6.service.interfaces;
 
 import java.util.List;
+
+import org.hibernate.Session;
+
 import ca.qc.collegeahuntsic.weblab6.dto.VitrineDTO;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidDTOException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidHibernateSessionException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidPrimaryKeyException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.InvalidSortByPropertyException;
 import ca.qc.collegeahuntsic.weblab6.exception.dao.ServiceException;
-import org.hibernate.Session;
+import ca.qc.collegeahuntsic.weblab6.exception.dao.VitrineAlreadyAddedException;
 
 public interface IVitrineService extends IService {
 
-    List<VitrineDTO> getAllVitrines(Session session,
-        String sortByPropertyName) throws InvalidHibernateSessionException,
-        InvalidSortByPropertyException,
-        ServiceException;
+	List<VitrineDTO> getAllVitrines(Session session, String sortByPropertyName)
+			throws InvalidHibernateSessionException,
+			InvalidSortByPropertyException, ServiceException;
 
-    void addVitrine(Session session,
-        VitrineDTO vitrineDTO) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        ServiceException;
+	void addVitrine(Session session, VitrineDTO vitrineDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ServiceException;
 
-    void deleteVitrine(Session session,
-        VitrineDTO vitrineDTO) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        ServiceException;
+	void deleteVitrine(Session session, VitrineDTO vitrineDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ServiceException;
 
-    void updateVitrine(Session session,
-        VitrineDTO vitrineDTO) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        ServiceException;
+	void updateVitrine(Session session, VitrineDTO vitrineDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			ServiceException;
 
-    VitrineDTO getVitrine(Session session,
-        String idVitrine) throws InvalidHibernateSessionException,
-        InvalidPrimaryKeyException,
-        ServiceException;
+	VitrineDTO getVitrine(Session session, String idVitrine)
+			throws InvalidHibernateSessionException,
+			InvalidPrimaryKeyException, ServiceException;
+
+	void ajouterVitrine(Session session, VitrineDTO vitrineDTO)
+			throws InvalidHibernateSessionException, InvalidDTOException,
+			VitrineAlreadyAddedException, ServiceException;
 
 }
