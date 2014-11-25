507f9536b426f8decbeb0f00f687e04217366445
charlesfarmer <200675851@collegeahuntsic.qc.ca>
2014-11-24 23:58:30 -0500

    sadf

---
+++ web lab 6 - front-end/src/ca/qc/collegeahuntsic/weblab6/servlet/action/ApplicationConfiguratorServlet.java
@@ -0,0 +1,61 @@
+// Fichier ApplicationConfiguratorServlet.java
+// Auteur : Gilles Bénichou
+// Date de création : 2014-09-06
+
+package ca.qc.collegeahuntsic.weblab6.servlet.action;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import ca.qc.collegeahuntsic.weblab6.Constants;
+import ca.qc.collegeahuntsic.weblab6.util.ApplicationConfigurator;
+
+/**
+ * Application configurator servlet. Loads the
+ * {@link ca.qc.collegeahuntsic.bibliotheque.util.ApplicationConfigurator}.
+ * 
+ * @author Gilles Benichou
+ */
+public class ApplicationConfiguratorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The logger.
+	 */
+	private static final Log LOGGER = LogFactory
+			.getLog(ApplicationConfiguratorServlet.class);
+
+	/**
+	 * Default constructor.
+	 */
+	public ApplicationConfiguratorServlet() {
+		super();
+	}
+
+	/**
+	 * Initializes the
+	 * {@link ca.qc.collegeahuntsic.bibliotheque.util.ApplicationConfigurator}.
+	 * 
+	 * @throws ServletException
+	 *             If an error occurs
+	 */
+	@Override
+	public void init() throws ServletException {
+		ApplicationConfigurator.init(getServletConfig().getInitParameter(
+				Constants.APPLICATION_CONFIGURATOR_INIT_PARAMETER_NAME));
+		ApplicationConfiguratorServlet.LOGGER
+				.info(Constants.APPLICATION_CONFIGURATOR_INIT_MESSAGE);
+	}
+
+	/**
+	 * Does nothing.
+	 */
+	@Override
+	public void destroy() {
+		ApplicationConfiguratorServlet.LOGGER
+				.info(Constants.APPLICATION_CONFIGURATOR_DESTROY_MESSAGE);
+	}
+}
